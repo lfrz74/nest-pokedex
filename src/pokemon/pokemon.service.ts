af6e79d07adea416ce77ad0e70d6f8ceae3e2112
lfrz <anonimus47@hotmail.com>
2023-08-29 15:27:15 -0500

    Adding Nest Config and joi package

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -6,6 +6,7 @@ import {
 } from "@nestjs/common";
 import { Model, isValidObjectId } from "mongoose";
 import { InjectModel } from "@nestjs/mongoose";
+import { ConfigService } from "@nestjs/config";
 
 import { CreatePokemonDto } from "./dto/create-pokemon.dto";
 import { UpdatePokemonDto } from "./dto/update-pokemon.dto";
@@ -14,10 +15,14 @@ import { PaginationDto } from "../common/dto/pagination.dto";
 
 @Injectable()
 export class PokemonService {
+  defaultLimit = 0;
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ) {}
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService
+  ) {
+    this.defaultLimit = configService.get<number>("defaultLimit");
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -32,7 +37,7 @@ export class PokemonService {
   }
 
   findAll(paginationDto: PaginationDto) {
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
     return this.pokemonModel
       .find()
       .limit(limit)
